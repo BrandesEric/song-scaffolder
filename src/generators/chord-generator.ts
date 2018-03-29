5b9eb70c4de2b9ac35db51bc5d0cf0df4ca3ecf6
Eric Brandes <eric.brandes@gmail.com>
2018-03-29 15:15:13 -0500

    Adding random chord progression generation as well

--- src/generators/chord-generator.ts
+++ src/generators/chord-generator.ts
@@ -1,18 +1,81 @@
 import { Phrase } from "../music/phrase";
-import * as tonal from "tonal";
+import * as Tonal from "tonal";
+import * as Key from "tonal-key";
 import { Note, NoteDuration } from "../music/note";
 import * as AbletonJs from "ableton-js";
 import { Track } from "ableton-js";
-import { IClipGenerationStrategy } from "../strategies/iclip-generation-strategy";
-import { SimpleChordStrategy } from "../strategies/chord-simple.strategy";
 import { ChordTrack } from "../state/tracks";
+import { Chord } from "../music/chord";
+import { SongConfig } from "../config/song.config";
+
+type ChordNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7;
+type ChordWeight = number;
+type WeightedChordNumber = [ChordNumber, ChordWeight]
+type WeightedChordMap = {
+    [key: string]: WeightedChordNumber[]
+}
 
 export class ChordGenerator {
 
     public chordTrack: ChordTrack;
+    private scale;
+    private chords;
+
+    private progressions = [
+        [1, 5, 6, 4],
+        [1, 4, 6, 5],
+        [1, 6, 4, 5],
+        [1, 5, 4, 5],
+        [4, 1, 5, 6],
+        [5, 6, 4, 1],
+        [6, 5, 4, 5],
+        [6, 4, 1, 5],
+    ];
+
+
+    majorChordStart: WeightedChordNumber[] = [[1, 60], [4, 10], [5, 10], [6, 20]];
+
+    majorChordMap: WeightedChordMap = {
+        "1": [
+            [1, 4],
+            [2, 2],
+            [3, 2],
+            [4, 10],
+            [5, 10],
+            [6, 10],
+        ],
+        "2": [
+            [3, 10],
+            [5, 10],
+            [1, 2],
+        ],
+        "3": [
+            [4, 10],
+            [6, 10],
+            [1, 4],
+        ],
+        "4": [
+            [2, 10],
+            [5, 10],
+            [1, 10],
+        ],
+        "5": [
+            [3, 10],
+            [6, 10],
+            [1, 10],
+        ],
+        "6": [
+            [4, 10],
+            [2, 10],
+            [1, 5],
+        ],
+    };
 
     constructor(chordTrack: ChordTrack) {
         this.chordTrack = chordTrack;
+        var scaleName = `${SongConfig.key} ${SongConfig.mode}`;
+        this.scale = Tonal.Scale.notes(scaleName);
+        this.chords = Key.chords(scaleName).map(x => x.replace("Maj7", "M").replace("m7", "m").replace("7", ""));
     }
 
     async generate() {
@@ -21,17 +84,154 @@ export class ChordGenerator {
             await this.clearClips(track)
         } 
         if(this.chordTrack.includeBasicChords){
-            await this.generateSimpleChords(track);
+            var phrases = this.generateBasicChordProgressions();
+            for(var i = 0; i < phrases.length; i++) {
+                await AbletonJs.insertMidiClip(track, phrases[i].toMidiClip());
+            } 
+        }
+        if(this.chordTrack.includeRandomChords){
+            for(var i = 0; i < this.chordTrack.numClips; i++) {
+                var phrase = this.generateRandomChordProgression();
+                await AbletonJs.insertMidiClip(track, phrase.toMidiClip());
+            }
         }
     }
  
-    async generateSimpleChords(track: Track){
-        var simpleChords = new SimpleChordStrategy(this.chordTrack)
-        var phrases = simpleChords.generateAll();
-        for(var i = 0; i < phrases.length; i++) {
-            await AbletonJs.insertMidiClip(track, phrases[i].toMidiClip());
+    generateRandomChordProgression(): Phrase {
+        var progression = [];
+        var startingChordNumber = this.getRandomChordStart();
+        var currentChordNumber = startingChordNumber; // I, ii, VI etc
+        progression.push(startingChordNumber);
+        for(var i = 0; i < 3; i++) {
+            var nextChordNumber = this.getRandomNextChordNumber(currentChordNumber)
+            progression.push(nextChordNumber);
+            currentChordNumber = nextChordNumber;
         }
         
+        return this.getChordPhraseWithRandomizedDuration(progression);
+    }
+
+    generateBasicChordProgressions(): Phrase[] {
+        var phrases = [];
+        for (var i = 0; i < this.progressions.length; i++) {
+            var progression = this.progressions[i];
+            var chordNames = progression.map(chordNumber => this.chords[chordNumber - 1]);
+            if(this.chordTrack.randomizeChordDuration){
+                var phrase = this.getChordPhraseWithRandomizedDuration(progression);
+                phrases.push(phrase);
+            }
+            else {
+                var chords = chordNames.map(chordName => new Chord(chordName, NoteDuration.Whole, this.chordTrack));
+                var phrase = new Phrase(progression.length, progression.toString());
+                phrase.addNotesFromChords(chords);
+                phrases.push(phrase);
+            }
+        }
+
+        return phrases;
+    }
+
+    private getChordPhraseWithRandomizedDuration(progression: number[]): Phrase {
+        var chordNames = progression.map(chordNumber => this.chords[chordNumber - 1]);
+        var currentClipLengthInBars = 0;
+        var progressionPosition = 0;
+        var chords = [];
+        while(currentClipLengthInBars < this.chordTrack.clipLengthInBars){
+            var duration = this.getRandomChordDuration();
+            var chord = new Chord(chordNames[progressionPosition % chordNames.length], duration, this.chordTrack)
+            chords.push(chord);
+            var shouldRepeatSameChord = this.getRandomInt(1, 100);
+            if(shouldRepeatSameChord > this.chordTrack.percentRepeatChance) {
+                progressionPosition++;
+            }
+            currentClipLengthInBars += duration.lengthInBars;
+        }
+
+        var phrase = new Phrase(this.chordTrack.clipLengthInBars, progression.toString())
+        phrase.addNotesFromChords(chords);
+
+        return phrase;
+    }
+
+    private getRandomChordStart(): ChordNumber {
+        var totalWeight = this.majorChordStart.reduce((accum, current) =>  current[1] + accum , 0);
+        var randomChance = this.getRandomInt(1, totalWeight);
+        for(var i = 0; i < this.majorChordStart.length; i++) {
+            if(randomChance <= this.majorChordStart[i][1]) {
+                return this.majorChordStart[i][0];
+            }
+            else {
+                randomChance -= this.majorChordStart[i][1];
+            }
+        }
+
+        throw new Error("Should always be able to find a starting chord!");
+    }
+
+    getRandomNextChordNumber(currentChord: ChordNumber): ChordNumber {
+        var weightedMap = this.majorChordMap[currentChord.toString()];
+        var totalWeight = weightedMap.reduce((accum, current) =>  current[1] + accum , 0);
+        var randomChance = this.getRandomInt(1, totalWeight);
+        for(var i = 0; i < weightedMap.length; i++) {
+            if(randomChance <= weightedMap[i][1]) {
+                return weightedMap[i][0];
+            }
+            else {
+                randomChance -= weightedMap[i][1];
+            }
+        }
+
+        throw new Error("Should always be able to find a next chord!");
+    }
+
+    private getRandomChordDuration(): NoteDuration {
+        var randomChance = this.getRandomInt(1, 100);
+        if(randomChance <= this.quarterNotePercentChance) {
+            return NoteDuration.Quarter
+        }
+        randomChance -= this.quarterNotePercentChance;
+        if(randomChance <= this.halfNotePercentChance) {
+            return NoteDuration.Half
+        }
+        randomChance -= this.halfNotePercentChance;
+        if(randomChance <= this.wholeNotePercentChance){
+            return NoteDuration.Whole;
+        }
+        randomChance -= this.wholeNotePercentChance;
+        if(randomChance <= this.twoWholeNotePercentChance){
+            return NoteDuration.TwoWhole;
+        }
+
+        throw new Error("ACK, should never not find a note duration!!");
+    }
+    
+    private get totalChances(): number {
+        return  this.chordTrack.quarterNoteChance + 
+            this.chordTrack.halfNoteChance +
+            this.chordTrack.wholeNoteChance +
+            this.chordTrack.twoWholeNoteChance
+    }
+
+    private get quarterNotePercentChance(): number {
+        return Math.floor((this.chordTrack.quarterNoteChance / this.totalChances) * 100);
+    }
+
+    private get halfNotePercentChance(): number {
+        return Math.floor((this.chordTrack.halfNoteChance / this.totalChances) * 100);
+    }
+
+    private get wholeNotePercentChance(): number {
+        return Math.floor((this.chordTrack.wholeNoteChance / this.totalChances) * 100);
+    }
+
+    private get twoWholeNotePercentChance(): number {
+        return Math.floor((this.chordTrack.twoWholeNoteChance / this.totalChances) * 100);
+    }
+
+    private getRandomInt(min: number, max: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
     }
 
     async clearClips(track: Track): Promise<void> {
--- src/state/tracks.ts
+++ src/state/tracks.ts
@@ -32,11 +32,12 @@ export class ChordTrack {
     twoWholeNoteChance: number = 0;
     percentRepeatChance: number = 0;
 
-    includeBasicChords: boolean = true;
+    includeBasicChords: boolean = false;
     clearClips: boolean = true;
     includeBassNote: boolean = true;
     includeTwoOctaveBassNote: boolean = false;
     randomizeChordDuration: boolean = false;
+    includeRandomChords: boolean = true;
 
     voicing: ChordVoicing = "normal";
     
@@ -60,6 +61,7 @@ export class ChordTrack {
         track.includeBassNote = !!form.includeBassNote && form.includeBassNote === "true";
         track.includeTwoOctaveBassNote = !!form.includeTwoOctaveBassNote && form.includeTwoOctaveBassNote === "true";
         track.randomizeChordDuration = !!form.randomizeChordDuration && form.randomizeChordDuration === "true";
+        track.includeRandomChords = !!form.includeRandomChords && form.includeRandomChords === "true";
         
         track.voicing = form.voicing;
 
